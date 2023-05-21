c61fef581c0ba099456a332d482cceaf53ef31b2
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-05-21 08:30:28 +0200

    Create reset password page route

---
+++ src/routes/ResetPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Reset from '../components/Reset/Reset'
+
+const ResetPage = () => {
+  return (
+    <div className='form--page__container'>
+        <Reset />
+    </div>
+  )
+}
+
+export default ResetPage
