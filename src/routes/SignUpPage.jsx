077bf2646b1fac354493dfe0d808498b0ad0a7f6
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-05-21 08:31:31 +0200

    Create sign up page route

---
+++ src/routes/SignUpPage.jsx
@@ -0,0 +1,11 @@
+import SignUp from "../components/SignUp/SignUp"
+
+const SignUpPage = () => {
+  return (
+    <div className="form--page__container">
+      <SignUp />
+    </div>
+  )
+}
+
+export default SignUpPage
