155eb70252150d0c2bd8c785d4e3f5cdfbfe2575
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-05-21 08:28:48 +0200

    Create login page

---
+++ src/routes/LoginPage.jsx
@@ -0,0 +1,11 @@
+import Login from "../components/Login/Login";
+
+const LoginPage = () => {
+  return (
+    <div className='form--page__container'>
+      <Login />
+    </div>
+  )
+}
+
+export default LoginPage
