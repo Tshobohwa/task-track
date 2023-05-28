6fd9231a4bd145046d1778ddf0fd6dcc2ff89fdb
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-05-28 08:08:08 +0200

    Add navbar to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import settingsReducer from "./settings/settingsSlice";
+import navbarReducer from "./navbar/navbarSlice";
 
 const store = configureStore({
   reducer: {
     settings: settingsReducer,
+    navbar: navbarReducer,
   },
 });
 
