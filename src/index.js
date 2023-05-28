265613c1079e40fea7d81721eedd4262d0cba6bb
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-05-28 08:22:00 +0200

    Wrap App into BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import store from "./redux/store";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
